3213ce1e15295a6ea0b560cfee7e6ca3e3691b66
markporo <markporo@yahoo.com>
2021-09-29 01:40:02 -0400

    episode component tests complete

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
 const testEpisode = {
-    id:1,
+    id: 1,
     name: "",
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
@@ -12,20 +12,45 @@ const testEpisode = {
     runtime: 1
 }
 
-const testEpisodeWithoutImage = {
-    //Add in approprate test data structure here.
+const testEpisodeForSummary = {
+    id: 1,
+    name: "",
+    image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
+    season: 1,
+    number: 1,
+    summary: "This is a dope episode",
+    runtime: 1
 }
 
-test("renders without error", () => {
+const testEpisodeWithoutImage = {
+    //Add in approprate test data structure here.
+    id: 1,
+    name: "",
+    image: null,
+    number: 1,
+    summary: "",
+    runtime: 1
+}
 
+it("renders without error", () => {
+    render(<Episode episode={testEpisode} />)
 });
 
-test("renders the summury test passed as prop", ()=>{
-    
+test("renders the summury test passed as prop", () => {
+    render(<Episode episode={testEpisodeForSummary} />)
+
+    const dopeSummary = screen.getByText(/This is a dope episode/i)
+
+    expect(dopeSummary).not.toBeNull()
+    expect(dopeSummary).not.toBe('this episode is not dope');
+    expect(dopeSummary).toBeInTheDocument()
 });
 
-test("renders default image when image is not defined", ()=>{
-    
+test("renders default image when image is not defined", () => {
+
+    render(<Episode episode={testEpisodeWithoutImage} />)
+    const defaultImage = screen.getByRole('img');
+    expect(defaultImage.alt).toBe('./stranger_things.png');
 })
 
 //Tasks
